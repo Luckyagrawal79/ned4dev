b809b6ed80c116754d637bdea9d3e5d1ced88d6e
Lucky Agrawal <luckyagrawal@Luckys-MacBook-Air.local>
2026-03-17 15:47:41 +0530

    Add liveintent to metric extractor

--- nlp/metric_extractor.py
+++ nlp/metric_extractor.py
@@ -1,6 +1,6 @@
 def extract_metric_from_query(q):
     metrics = []
-    for m in ['maid','gravy','cookie','dig']:
+    for m in ['maid','gravy','cookie','dig', 'liveintent']:
         if m in q.lower():
             metrics.append(m.capitalize())
     return metrics
