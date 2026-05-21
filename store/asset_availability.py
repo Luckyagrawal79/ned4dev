521d0fb050bd96b474ae2f5ce50e5f25da367529
Lucky Agrawal <luckyagrawal@Luckys-MacBook-Air.local>
2026-05-21 11:14:28 +0530

    Asset Availability path update part 1

--- store/asset_availability.py
+++ store/asset_availability.py
@@ -9,33 +9,188 @@ from google.cloud import storage
 ASSET_PATHS = {
     "truedata": {
         "path": "gs://fe8f7b53-6cab-4a29-8efa-831ed8849353-2183-a/",
-        "display": "TrueData",
+        "display": "Truedata",
         "cadence": "daily",
+    },
+    "nextglass": {
+        "path": "gs://8bfec105-8abc-4af1-9c8d-e4d42cade844-2252-a/",
+        "display": "Nextglass",
+        "cadence": "monthly",
+    },
+    "unique_maid_list": {
+        "path": "gs://fdaa9419-fd0b-44fa-9584-d060848c4caf-4373-a/",
+        "display": "Unique MAID List",
+        "cadence": "weekly",
+    },
+    "ccpa_online": {
+        "path": "gs://898cc58e-0bb1-4031-a751-2e1154834da4-2464-s/",
+        "display": "CCPA Online",
+        "cadence": "monthly",
+    },
+    "ipi_ipv4": {
+        "path": "gs://7395e23a-bb60-41f4-af0d-6754557cb538-2521-a/",
+        "display": "IPI IPv4",
+        "cadence": "weekly",
+    },
+    "ibotta": {
+        "path": "gs://a255864d-c9e5-41bc-9fc1-ed58b6fa5c3d-2518-a/",
+        "display": "Ibotta",
+        "cadence": "monthly",
+    },
+    "startapp": {
+        "path": "gs://e9c43575-43c6-49fd-9307-daef4d8ecd01-2571-a/",
+        "display": "Startapp",
+        "cadence": "daily",
+    },
+    "zeta": {
+        "path": "gs://a48eaa4d-2c47-4938-b75d-205fc7173f6d-2934-a/",
+        "display": "Zeta",
+        "cadence": "monthly",
+    },
+    "truedata_ctv_raw": {
+        "path": "gs://89e46ed9-b4f0-468f-97a3-80cba59b403b-4934-a/",
+        "display": "TrueData CTV Raw",
+        "cadence": "weekly",
+    },
+    "truedata_ctv": {
+        "path": "gs://4980a90f-a83b-4565-bce5-dabd5cd38339-2652-a/",
+        "display": "Truedata CTV",
+        "cadence": "weekly",
+    },
+    "maf": {
+        "path": "gs://05a40216-9391-40f5-a787-8caa68ab9319-2284-a/",
+        "display": "MAF",
+        "cadence": "monthly",
     },
     "adadvisor": {
         "path": "gs://a6c6cfa9-9556-4504-8d31-dcd47d5aaa8a-2499-a/",
         "display": "AdAdvisor",
         "cadence": "daily",
     },
-    "truedata-ctv": {
-        "path": "gs://4980a90f-a83b-4565-bce5-dabd5cd38339-2652-a/",
-        "display": "TrueData CTV",
-        "cadence": "weekly",
-    },
-    "liv": {
-        "path": "gs://457bb09e-1849-4683-81ac-989ca9ec29da-6252-a/",
-        "display": "LiveIntent",
-        "cadence": "daily",
-    },
-    "gry": {
+    "gravy": {
         "path": "gs://7e846dd6-16a9-4bab-af56-f639a3f8f383-2516-a/",
         "display": "Gravy",
         "cadence": "daily",
     },
-    "audacq": {
+    "arin_ipv6": {
+        "path": "gs://8569d6d0-df9a-4243-b6e1-55b2d19da11e-5450-a/",
+        "display": "ARIN IPV6",
+        "cadence": "weekly",
+    },
+    "arin_ipv4": {
+        "path": "gs://553c55ca-1b66-46f9-9411-0c979d94ffea-5473-a/",
+        "display": "ARIN IPV4",
+        "cadence": "weekly",
+    },
+    "ipi_ipr": {
+        "path": "gs://46cda202-9adb-4c76-8578-c4c036bd5a8a-2517-a/",
+        "display": "IPI IPR",
+        "cadence": "daily",
+    },
+    "stirista_ctv": {
+        "path": "gs://ef07a99f-a4b0-4640-8ee6-2aa8f2c639a8-5139-a/",
+        "display": "Stirista CTV",
+        "cadence": "daily",
+    },
+    "kdi": {
+        "path": "gs://31fb003f-58c1-4e03-a402-4f4e56eff2d5-2949-a/",
+        "display": "KDI",
+        "cadence": "monthly",
+    },
+    "ventive_iq": {
+        "path": "gs://648d891a-0fbd-4d10-890c-44afbad09b26-5599-a/",
+        "display": "Ventive IQ",
+        "cadence": "weekly",
+    },
+    "liveintent": {
+        "path": "gs://457bb09e-1849-4683-81ac-989ca9ec29da-6252-a/",
+        "display": "LiveIntent",
+        "cadence": "daily",
+    },
+    "third_party_internal_email": {
+        "path": "gs://0d9f08cb-ab81-4524-8765-ad1a21722431-3212-a/",
+        "display": "Third Party Internal Email",
+        "cadence": "weekly",
+    },
+    "third_party_internal_phones": {
+        "path": "gs://65dfd896-4f6d-4f60-a6e3-edc5d906b4ca-3213-a/",
+        "display": "Third Party Internal Phones",
+        "cadence": "weekly",
+    },
+    "third_party_id5_hem": {
+        "path": "gs://79f36342-a1a9-4bd9-b14c-8ffeca89fdbb-5379-a/",
+        "display": "Third Party ID5 HEM",
+        "cadence": "weekly",
+    },
+    "third_party_ramp_id": {
+        "path": "gs://f85d388b-a710-4342-bd8b-484602bbb6c5-5880-a/",
+        "display": "Third Party Ramp ID",
+        "cadence": "weekly",
+    },
+    "third_party_tradedesk": {
+        "path": "gs://354e9396-3379-4a3e-a567-0ce8b15eb3ed-5216-a/",
+        "display": "Third Party TradeDesk",
+        "cadence": "weekly",
+    },
+    "ipi_ipv6": {
+        "path": "gs://81e7e936-53e4-4432-b46a-29949eaca4ac-5335-a/",
+        "display": "IPI IPV6",
+        "cadence": "weekly",
+    },
+    "ccpa_offline": {
+        "path": "gs://1850aac2-fd3d-48e3-b6ac-588cb80389aa-2305-a/",
+        "display": "CCPA Offline",
+        "cadence": "monthly",
+    },
+    "positive_ccpa_optout": {
+        "path": "gs://7c7d8ec5-8aa2-43a7-81d4-2e51ab2dbe99-5350-a/",
+        "display": "Positive CCPA Optout",
+        "cadence": "monthly",
+    },
+    "offline_enl_blend": {
+        "path": "gs://450a97d6-9885-4ec6-b7a7-46882570026c-2293-a/",
+        "display": "Offline ENL Blend",
+        "cadence": "monthly",
+    },
+    "identity_events": {
+        "path": "gs://927bbfd7-25b4-4ac6-8a0c-a05606c89d47-2525-a/",
+        "display": "Identity Events",
+        "cadence": "daily",
+    },
+    "offline_enl_phone": {
+        "path": "gs://c75a2461-1f69-4f5a-afd2-357577b3ab3c-2310-a/",
+        "display": "Offline ENL Phone",
+        "cadence": "monthly",
+    },
+    "ipv4_ipv6_mapping_table": {
+        "path": "gs://874b63dd-fd8e-4303-8d72-89aed43bf606-2707-a/",
+        "display": "IPV4 IPV6 Mapping Table",
+        "cadence": "weekly",
+    },
+    "offline_email_ekey_pid": {
+        "path": "gs://81e41181-68c1-4d83-8295-a6c329226e19-2309-a/",
+        "display": "Offline Email Ekey Pid",
+        "cadence": "monthly",
+    },
+    "offline_enl_canonical": {
+        "path": "gs://0b6a9808-9df0-4537-80fe-d5afd50c830e-2297-a/",
+        "display": "Offline ENL Canonical",
+        "cadence": "monthly",
+    },
+    "offline_enl_ekey_tier": {
+        "path": "gs://99effa63-f25e-43a0-a303-4384036e9610-2308-a/",
+        "display": "Offline ENL Ekey Tier",
+        "cadence": "monthly",
+    },
+    "audience_acuity": {
         "path": "gs://54fdbe34-9fa9-4262-8110-de5fba26b4ea-3775-a/",
         "display": "Audience Acuity",
         "cadence": "daily",
+    },
+    "ipi_ipv4_pp": {
+        "path": "gs://c2dbcd0e-c11f-4c94-8556-78e31c82e8b1-2981-a/",
+        "display": "IPI IPv4 PP",
+        "cadence": "weekly",
     },
 }
 
